24d5061bff5f3715718fc8ec52b875bad1aa7fec
Aditya Saripalli <aditya.saripalli@students.iiit.ac.in>
2018-11-06 23:50:22 +0530

    main() modified to take files as input and parse.

--- src/Driver/DecafParser.cc
+++ src/Driver/DecafParser.cc
@@ -5,34 +5,41 @@
 #include "Driver.hh"
 #include "Modules.hh"
 #include "AstVisitor.hh"
+using namespace std;
 
-int main()
+int main(int argc, char *argv[])
 {
     AstContext sAstCtx;
     decaf::Driver sDriver(sAstCtx);
     AstVisitor sVisitor;
-    std::string sLine;
-    Program* pProg;
 
-    while(1)
+    for(int index = 1; index < argc; index++)
     {
-        std::cout << "Input: " << std::flush;
-        getline(std::cin, sLine);
-        if (!sLine.empty())
+	    // read a decaf program
+	    fstream infile(argv[index]);
+	    if (!infile.good())
+	    {
+		    cerr << "Could not open file: " << argv[index] << endl;
+		    return 0;
+	    }
+
+        cout << "Parsing " << argv[index] << " Started" << endl;
+
+	    //sAstCtx.clearAST();
+        //cout << "AST Cleared." << endl;
+
+	    bool result = sDriver.parse_stream(infile, argv[index]);
+        if (result)
         {
-            bool result = sDriver.parse_string(sLine, "Input");
-            if (result)
+            cout << "Parsing " << argv[index] << " Successful" << endl;
+            if (sAstCtx.pRoot != NULL )
             {
-                if (sAstCtx.pRoot != NULL )
-                {
-                    std::cout << "Traversing the AST: " << std::endl;
-                    pProg = dynamic_cast<Program*>(sAstCtx.pRoot);
-                    pProg->accept(sVisitor);
-                    std::cout << "Traverse AST Complete !!!" << std::endl; 
-                }
-                sAstCtx.clearAST();
+                cout << "Traversing the AST ..." << endl;
+                sAstCtx.pRoot->accept(sVisitor);
+                cout << "Traverse AST Complete !!!" << endl; 
             }
         }
 	}
+    //sAstCtx.clearAST();
 }
 
