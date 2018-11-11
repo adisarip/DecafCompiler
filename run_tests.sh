b0426d5966586bb5fc268a0f8d2a56d4ba236bbb
Aditya Saripalli <aditya.saripalli@students.iiit.ac.in>
2018-11-11 23:46:58 +0530

    added script to run tests and also run_tests.log

---
+++ run_tests.sh
@@ -0,0 +1,16 @@
+
+echo "Parsing ./test/fact.dcf"
+./bin/decaf ./test/fact.dcf
+
+echo "..."
+echo "..."
+
+echo "Parsing ./test/fib.dcf"
+./bin/decaf ./test/fib.dcf
+
+echo "..."
+echo "..."
+
+echo "Parsing ./test/hanoi.dcf"
+./bin/decaf ./test/hannoi.dcf
+
